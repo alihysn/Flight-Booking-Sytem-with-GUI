ecbbb9c9fd3528b44c68423115b9b40b56c202af
Mohamed Hashish <mohamedh_4@aucegypt.edu>
2021-05-17 23:28:25 +0200

    getting data from sql after sorting

--- sflights.cpp
+++ sflights.cpp
@@ -20,7 +20,23 @@ SFlights::~SFlights()
 
 void SFlights::on_pushButton_clicked()
 {
+    QMessageBox box;
     QVector<float> final;
     Search *searcher = new Search();
     final = searcher->SearchByPrice();
+    QSqlDatabase database;
+    database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("/media/mohamed/01D674211A1C18801/AUC/Freshman/Spring 2021/CSCE1101-03 - Fundamentals of Computing II/Project/ProjectDB");
+    database.open();
+    QSqlQuery query;
+    query.prepare("SELECT Airline, Price FROM Flights WHERE Price=10");
+    query.exec();
+    QString test;
+    while (query.next())
+    {
+        test = query.value(0).toString();
+    }
+    box.setWindowTitle(test);
+    box.exec();
+    database.close();
 }
