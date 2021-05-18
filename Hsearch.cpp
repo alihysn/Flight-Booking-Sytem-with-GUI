2ac1523ecc2c5298272b4a1efe58eaff911762da
abdallahgs <abdallah184@aucegypt.edu>
2021-05-18 22:31:52 +0200

    editzz

--- Hsearch.cpp
+++ Hsearch.cpp
@@ -4,6 +4,7 @@
 #include "search.h"
 #include<QtSql/QSqlDatabase>
 #include<QtSql/QSqlQuery>
+#include <QString>
 
 Hsearch::Hsearch(QWidget *parent) :
     QDialog(parent),
@@ -69,10 +70,49 @@ void Hsearch::on_pushButton_2_clicked()
 
 void Hsearch::on_pushButton_3_clicked()
 {
-
+    QMessageBox box;
+    QVector<float> final;
+    Search *searcher = new Search();
+    final = searcher->SearchByDistancefromairport();
+    QSqlDatabase database;
+    database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("/media/mohamed/01D674211A1C18801/AUC/Freshman/Spring 2021/CSCE1101-03 - Fundamentals of Computing II/Project/ProjectDB");
+    database.open();
+    QSqlQuery query;
+    query.prepare("SELECT Price, Rating FROM Flights WHERE DistanceFromAirport = final");
+    query.exec();
+    QString test;
+    while (query.next())
+    {
+        test = query.value(0).toString();
+    }
+    box.setWindowTitle("hotels by distance");
+    box.setText(test);
+    box.exec();
+    database.close();
 }
 
 void Hsearch::on_pushButton_4_clicked()
 {
 
+    QMessageBox box;
+    QVector <QString>  city;
+    //Search *searcher = new Search();
+    //final = searcher->SearchByRatingH();
+    QSqlDatabase database;
+    database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("/media/mohamed/01D674211A1C18801/AUC/Freshman/Spring 2021/CSCE1101-03 - Fundamentals of Computing II/Project/ProjectDB");
+    database.open();
+    QSqlQuery query;
+    query.prepare("SELECT Price, Rating FROM Hotels WHERE location = city");
+    query.exec();
+    QString test;
+    while (query.next())
+    {
+        test = query.value(0).toString();
+    }
+    box.setWindowTitle("hotels by cities");
+    box.setText(test);
+    box.exec();
+    database.close();
 }
