b5b5cf420a58bc15317102e3100e7a195584ca69
abdallahgs <abdallah184@aucegypt.edu>
2021-05-19 04:42:02 +0200

    halfdjikstra

--- findshortestpath.h
+++ findshortestpath.h
@@ -1,11 +1,15 @@
 #ifndef FINDSHORTESTPATH_H
 #define FINDSHORTESTPATH_H
-
-
+#include <QVector>
+#include <QVector2D>
+#include <climits>
+using namespace std;
 class FindShortestPath
 {
+    QVector2D graph(5,5);
 public:
-    FindShortestPath();
+    int mindistance();
+    void FindShortestPath();
 };
 
 #endif // FINDSHORTESTPATH_H
